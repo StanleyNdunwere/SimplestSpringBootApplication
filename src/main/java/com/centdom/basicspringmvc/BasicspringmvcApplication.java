087e98dc033686159bba962483f9a97cffb9df33
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2020-01-05 10:58:35 +0100

    simplest Spring MVC application

---
+++ src/main/java/com/centdom/basicspringmvc/BasicspringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.centdom.basicspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicspringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicspringmvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/centdom/basicspringmvc/BasicspringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centdom.basicspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicspringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
